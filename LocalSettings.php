96f10eec183fd64a4e339d22ceceb5b03b1c79e6
Virginia Cepeda <vcepeda@despegar.com>
2016-04-23 12:51:00 -0300

    Cambiar logo

--- LocalSettings.php
+++ LocalSettings.php
@@ -43,7 +43,7 @@ $wgResourceBasePath = $wgScriptPath;
 
 ## The URL path to the logo.  Make sure you change this from the default,
 ## or else you'll overwrite your logo when you upgrade!
-$wgLogo = "$wgResourceBasePath/resources/assets/logoFeo.png";
+$wgLogo = "$wgResourceBasePath/resources/assets/logoNodos.png";
 
 ## UPO means: this is also a user preference option
 
